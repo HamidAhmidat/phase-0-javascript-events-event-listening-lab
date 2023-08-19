c69552a3b029a75ab7b9f3a3bcd642ad177fcb69
hamid <ahmidatabdelhamid@gmail.com>
2023-08-19 19:03:04 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -4,13 +4,3 @@ function addingEventListener() {
     alert("I was clicked!");
   });
 }
-
-addingEventListener();
-function addingEventListener() {
-  const input = document.getElementById("button");
-  input.addEventListener("click", function () {
-    alert("I was clicked!");
-  });
-}
-
-addingEventListener();
